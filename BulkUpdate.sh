98db61922cd1bf26874f1e9672c3b893f42d2076
munrav84 <33266493+munrav84@users.noreply.github.com>
2021-01-24 00:09:42 -0500

    Update BulkUpdate.sh

--- BulkUpdate.sh
+++ BulkUpdate.sh
@@ -4,10 +4,10 @@ cp ignore.csv
 
 while IFS=$',\r' read -r field1 field2 field3;
 do
-	printf "UPDATE ODS.PERF_ACCT_ERR_DTL SET REC_ACTV_IND='T' WHERE BUS_DT='$field3' and ETL_ERR_CD='$field2' AND ACCT_KEY in (SELECT ACCT_KEY FROM ODS.ACCT_PORTFOLIO WHERE ACCT_SRCH_NBR='$field1');\nCOMMIT;\n"
+	printf "UPDATE OBS.ERR_DTL SET REC_ACTV_IND='T' WHERE BUS_DT='$field3' and ETL_ERR_CD='$field2' AND ACCT_KEY in (SELECT ACCT_KEY FROM OBS.ACCT_PORTFOLIO WHERE ACCT_SRCH_NBR='$field1');\nCOMMIT;\n"
    done < IGNORES.csv > queryfile.sql
 
-. /opt/CA/WorkloadAutomationAE/SystemAgent/WA1_AGENT/profiles/dst.profile
+. /opt/CA/WorkloadAutomationAE/SystemAgent/WA1_AGENT/profiles
 
 now=`date +"%Y%m%d"`
 echo "Starting of sqlplus"
